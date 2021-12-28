5666b7ba53c534fd3b5062eb1d9b1f0820af1761
Quentin Balin <quentin.balin@gusto.com>
2021-12-28 16:19:42 -0500

    Add other etherscan-like clients

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,8 @@
 import KucoinClient from './api_clients/kucoin';
 import EtherscanClient from './api_clients/etherscan';
+import PolygonscanClient from './api_clients/polygonscan';
+import BscscanClient from './api_clients/bscscan';
+import CoinbaseClient from './api_clients/coinbase';
 import CoingeckoClient from './api_clients/coingecko';
 import { SupportedBlockchain, SupportedPlatform } from './constants';
 import Kucoin from './attributes/kucoin';
@@ -8,4 +11,4 @@ interface Attributes {
     Kucoin: Kucoin;
     EtherscanLike: EtherscanLike;
 }
-export { KucoinClient, CoingeckoClient, EtherscanClient, SupportedBlockchain, SupportedPlatform, Attributes };
+export { KucoinClient, CoingeckoClient, PolygonscanClient, BscscanClient, CoinbaseClient, EtherscanClient, SupportedBlockchain, SupportedPlatform, Attributes };
