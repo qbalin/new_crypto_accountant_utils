d763ac2eb7103aa809023e0b76aa5ee94ad65a9c
Quentin Balin <quentin.balin@gusto.com>
2022-01-02 13:57:19 -0500

    Let the package export TS

--- dist/api_clients/polygonscan.d.ts
+++
@@ -1,7 +0,0 @@
-import EtherscanBaseClient from './etherscan_like';
-declare class Client extends EtherscanBaseClient {
-    constructor({ etherscanLikeApiKey }: {
-        etherscanLikeApiKey: string;
-    });
-}
-export default Client;
--- dist/attributes/kucoin/index.d.ts
+++
@@ -1,5 +0,0 @@
-import LedgerEntry from "./ledger_entry";
-interface KucoinAttributes {
-    LedgerEntry: LedgerEntry;
-}
-export default KucoinAttributes;
